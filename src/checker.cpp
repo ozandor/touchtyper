914c58b799cef739042bd4df8abd090a1a67a3cb
ozandor <ozandor@users.noreply.github.com>
2020-12-23 21:54:04 +0300

    checker
    checker ve letter iskeleti

---
+++ src/checker.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+//dersin içeriğini tutan fonksiyon
+string letter(int x){
+    if(x=1){
+
+    }
+    else if(x=2){
+
+    }
+    else if(x=3){
+        
+    }
+    else if(x=4){
+        
+    }
+    else if(x=5){
+        
+    }
+    else if(x=6){
+        
+    }
+    else if(x=7){
+        
+    }
+    else if(x=8){
+        
+    }
+    else if(x=9){
+        
+    }
+    else if(x=10){
+        
+    }
+    else if(x=11){
+        
+    }
+    else if(x=12){
+        
+    }
+    else if(x=13){
+        
+    }
+    else if(x=14){
+        
+    }
+    else if(x=15){
+        
+    }
+
+
+}
+//bu iki fonksiyon yok yazılması lazım
+bool checker(letterexpected(),inputletter()){
+    if(letter()=inputletter()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
