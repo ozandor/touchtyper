dca6cedb16ad0bf247fa3e594dcd813487eccf3a
bozender <b.ozender59@gmail.com>
2020-12-24 23:58:31 +0300

    created ChallengeBox(not finished)

---
+++ src/ChallengeBox.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+//#include "Mechanic.h"
+/*
+using namespace std;
+
+class ChallengeBox{
+    public:    
+        ChallengeBox(sf::RenderWindow* win, wstring chl_str);
+        ~ChallengeBox();
+        void Calculate();
+        void DrawText();
+
+    private:
+        inline int GetCharLength(char &letter, sf::Font &font, int font_size);
+        wstring challenge_str;
+        sf::RenderWindow* window = NULL;
+        sf::Text txt;
+}; 
+*/ 
