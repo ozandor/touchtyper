9951fae83e4d302c1ef2c727e648caa673c9922d
ozandor <ozandor@users.noreply.github.com>
2020-12-24 22:17:34 +0300

    Mechanic.h and box's last string
    mechanic.h's checker part is not done yet. The programs randomized last string has done.

---
+++ src/Mechanic.h
@@ -0,0 +1,23 @@
+/*
+//#include <stdio.h>
+
+
+//bu iki fonksiyon yok yazılması lazım(CHECKER BU)
+bool checker(letterexpected(),inputletter()){
+    if(letterexpected()==inputletter()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+//kullanıcıya bu harfleri bas diyoruz
+char letterexpected(){
+
+}
+
+//kullanıcının bastığı 
+char inputletter(){
+
+} */
