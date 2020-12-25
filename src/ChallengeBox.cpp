a93f5e655ab967662c07da89987967e3b6da478f
bozender <b.ozender59@gmail.com>
2020-12-25 17:31:56 +0300

    ChallengeBox continued

--- src/ChallengeBox.cpp
+++ src/ChallengeBox.cpp
@@ -1,2 +1,13 @@
 #include "ChallengeBox.h"
 
+ChallengeBox::ChallengeBox(sf::RenderWindow* win, sf::Font fnt, wstring challenge_str, bool is_dark){
+    window = win;
+    txt.setFont(fnt);
+    txt.setString(challenge_str);
+    if(is_dark){
+        txt.setFillColor(sf::Color(192,192,192));
+    }else{
+        txt.setFillColor(sf::Color::Black);
+    }
+}
+
--- src/ChallengeBox.h
+++ src/ChallengeBox.h
@@ -2,20 +2,22 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 //#include "Mechanic.h"
-/*
+
 using namespace std;
 
 class ChallengeBox{
     public:    
-        ChallengeBox(sf::RenderWindow* win, wstring chl_str);
+        ChallengeBox(sf::RenderWindow* win, sf::Font fnt, wstring challenge_str, bool is_dark);
         ~ChallengeBox();
-        void Calculate();
-        void DrawText();
+        void Calculate(bool is_correct);
+        void DrawChallenge();
+
 
     private:
         inline int GetCharLength(char &letter, sf::Font &font, int font_size);
-        wstring challenge_str;
+        
         sf::RenderWindow* window = NULL;
         sf::Text txt;
+    //    Mechanic mec;
+    
 }; 
-*/ 
